b49c565a524a491ec1efc3297bc852103e2d0cc3
Xavier Basty <xbasty@gmail.com>
2013-12-11 10:24:47 +0100

    Added project properties

---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "dst-amqp"
+  val appVersion      = "1.0"
+
+  val appDependencies = Seq(
+    "com.rabbitmq" % "amqp-client" % "2.8.1"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    scalaVersion := "2.10.2",
+    organization := "se.destination",
+    sources in doc in Compile := List(),
+    scalacOptions ++= Seq("-feature", "-language:postfixOps"),
+
+    resolvers += "Typesafe" at "http://repo.typesafe.com/typesafe/repo",
+    resolvers += "Maven Repository" at "http://repo1.maven.org/maven2/org/"
+  )
+}
