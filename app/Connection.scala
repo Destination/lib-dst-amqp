dc8f167eb4fe18c24552bbc497de4df75a2d9c4d
Xavier Basty <xbasty@gmail.com>
2013-12-19 15:27:29 +0100

    Made hostname optional in connection contructor

--- app/Connection.scala
+++ app/Connection.scala
@@ -4,19 +4,24 @@ import com.rabbitmq.client.ConnectionFactory
 
 class Connection(connectionFactory: ConnectionFactory = new ConnectionFactory()) {
   def this(
-    hostName:     String,
-    portNumber:   Option[Int] = None,
-    virtualHost:  Option[String] = None,
-    userName:     Option[String] = None,
-    password:     Option[String] = None
+    hostName:     Option[String] =  None,
+    portNumber:   Option[Int] =     None,
+    virtualHost:  Option[String] =  None,
+    userName:     Option[String] =  None,
+    password:     Option[String] =  None
   ) = this({
     val factory = new ConnectionFactory()
-    factory.setHost(hostName);
+    hostName.map(factory.setHost(_));
     portNumber.map(factory.setPort(_))
     virtualHost.map(factory.setVirtualHost(_))
     userName.map(factory.setUsername(_))
     password.map(factory.setPassword(_))
 
+    factory
+  })
+  def this(uri: String) = this({
+    val factory = new ConnectionFactory()
+    factory.setUri(uri)
     factory
   })
 
