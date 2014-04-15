5c514f65febfeeb529274643852b0f49fdbffffe
Xavier Basty <xbasty@gmail.com>
2014-04-15 10:48:24 +0200

    Added publish methods with mandatory and immediate flags

--- app/Exchange.scala
+++ app/Exchange.scala
@@ -6,12 +6,22 @@ import com.rabbitmq.client.{
 }
 
 class Exchange(val name: String, channel: RMQChannel) {
+  private final val defaultEncoding = "UTF-8"
+
   def publish(routingKey: String, message: String) {
-    publish(routingKey, message, "UTF-8")
+    publish(routingKey, message, defaultEncoding)
   }
 
   def publish(routingKey: String, message: String, encoding: String) {
     publish(routingKey, message.getBytes(encoding))
+  }
+
+  def publish(routingKey: String, message: String, mandatory: Boolean, immediate: Boolean) {
+    publish(routingKey, message, defaultEncoding)
+  }
+
+  def publish(routingKey: String, message: String, mandatory: Boolean, immediate: Boolean, encoding: String) {
+    publish(routingKey, message, encoding)
   }
 
   def publish(routingKey: String, message: Array[Byte], mandatory: Boolean = false, immediate: Boolean = false) {
