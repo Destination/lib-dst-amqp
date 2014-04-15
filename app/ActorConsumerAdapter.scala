f3bed0d506b92a9c8d09b1d0a068e8ac518d58c3
Xavier Basty <xbasty@gmail.com>
2014-04-15 13:30:26 +0200

    Better trace messages in consumer adapter

--- app/ActorConsumerAdapter.scala
+++ app/ActorConsumerAdapter.scala
@@ -25,47 +25,48 @@ class ActorConsumerAdapter(consumer: ActorRef, queue: Queue) extends ActorConsum
   def onReceive (message: Any, sender: ActorRef) = {
     message match {
       case msg @ Ack(deliveryTag, multiple) => {
-        Logger.trace(s"Acknowleging  message #${deliveryTag}: '${msg}'")
+        Logger.trace(s"{consumer.path} - Acknowleging message #${deliveryTag}: '${msg}'")
         queue.channel.basicAck(deliveryTag, multiple)
       }
       case msg @ Reject(deliveryTag, requeue) => {
-        Logger.trace(s"Rejecting message #${deliveryTag}: '${msg}', requeuing: ${requeue}")
+        Logger.trace(s"{consumer.path} - Rejecting message #${deliveryTag}: '${msg}', requeuing: ${requeue}")
         queue.channel.basicReject(deliveryTag, requeue)
       }
       case Accept(deliveryTag) => {
-        Logger.trace(s"Message #{deliveryTag} has been accepted by recipient.")
+        Logger.trace(s"Message #{deliveryTag} has been accepted by {consumer.path}.")
       }
     }
   }
 
   def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]) {
-    Logger.trace(s"Received message from queue: ${queue.name}")
-    consumer ! IncomingMessage(consumerTag, envelope, properties, new String(body, "UTF-8"))
+    val message = new String(body, "UTF-8")
+    Logger.trace(s"{consumer.path} - Received message from queue '${queue.name}': ${message}")
+    consumer ! IncomingMessage(consumerTag, envelope, properties, message)
   }
 
   override def handleConsumeOk(consumerTag: String) {
     _consumerTag = consumerTag
-    Logger.trace("Consume ok received, informing actor")
+    Logger.trace(s"${consumer.path} - Consume ok received (${consumerTag})")
     consumer ! ConsumeOk(consumerTag)
   }
 
   override def handleCancelOk(consumerTag: String) {
-    Logger.trace("Cancel ok received, informing actor")
+    Logger.trace("{consumer.path} - Cancel ok received, informing actor")
     consumer ! CancelOk(consumerTag)
   }
 
   override def handleCancel(consumerTag: String) {
-    Logger.trace("Cancel received, informing actor")
+    Logger.trace("{consumer.path} - Cancel received, informing actor")
     consumer ! Cancel(consumerTag)
   }
 
   override def handleRecoverOk(consumerTag: String) {
-    Logger.trace("Recover ok received, informing actor")
+    Logger.trace("{consumer.path} - Recover ok received, informing actor")
     consumer ! RecoverOk(consumerTag)
   }
 
   override def handleShutdownSignal(consumerTag: String, sig: ShutdownSignalException) {
-    Logger.trace("Connection shutdown, informing actor")
+    Logger.trace("{consumer.path} - Connection shutdown, informing actor")
     consumer ! ShutdownSignal(consumerTag, sig)
   }
 }
